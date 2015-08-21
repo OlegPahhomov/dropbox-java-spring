87e52fdc12a7b747ac30a317722ddb0e1beda7fd
oleg.pahhomov@gmail.com <lol98nuj>
2015-08-21 17:46:19 +0300

    inital config, copied from spark, will refactor to use DI asap

---
+++ src/main/java/files/IndexController.java
@@ -0,0 +1,39 @@
+package files;
+
+import com.google.gson.Gson;
+import org.apache.commons.dbutils.QueryRunner;
+import org.apache.commons.dbutils.handlers.MapListHandler;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Controller
+@EnableAutoConfiguration
+public class IndexController {
+
+    static QueryRunner queryRunner = new QueryRunner();
+
+    @RequestMapping("/")
+    @ResponseBody
+    String home() {
+        return "Hello World!";
+    }
+
+    @RequestMapping("/files")
+    @ResponseBody
+    String getFiles() throws SQLException {
+        try (Connection connection = AppDataSource.getConnection()) {
+            return new Gson().toJson(queryRunner.query(connection, "SELECT *, IMAGE_WIDTH::float / IMAGE_HEIGHT AS RATIO FROM FILE", new MapListHandler()));
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(IndexController.class, args);
+    }
+}
